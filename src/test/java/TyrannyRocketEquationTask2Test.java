68e0ac5f031368ccf9724df83f22cc2d1a4136d8
Roman Schuller <roman.schuller@gmail.com>
2022-03-24 23:02:18 +0100

    fixed Annotations

--- src/test/java/TyrannyRocketEquationTask2Test.java
+++ src/test/java/TyrannyRocketEquationTask2Test.java
@@ -1,5 +1,5 @@
-import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,7 +15,7 @@ public class TyrannyRocketEquationTask2Test {
 
     // Übergebe der Methode eine negative Zahl
     @Test
-    @Description("Call Method with negative Numbers")
+    @DisplayName("Call Method with negative Numbers")
     void fuelCalculatorTest_MinusParameters(){
         Integer actual = TyREObj2.fuelCalculator(-2);
         String expected = "ERROR: Mass is under 0";
@@ -25,21 +25,21 @@ public class TyrannyRocketEquationTask2Test {
 
     // Übergebe der Methode eine Zahlen/Buchstaben Mischung
     @Test
-    @Description("Call Method with Numbers/Text Mix")
+    @DisplayName("Call Method with Numbers/Text Mix")
     void fuelCalculatorTest_Parameters(){
 
 
     }
     // Übergebe der Methode einen String
     @Test
-    @Description("Call Method a String as Parameter")
+    @DisplayName("Call Method a String as Parameter")
     void fuelCalculatorTest_StringParameters(){
 
 
     }
     // Übergebe der Methode Null
     @Test
-    @Description("Call Method with Null as Parameter")
+    @DisplayName("Call Method with Null as Parameter")
     void sumofCalcsTest_Sum654PLUS33583(){
         TyREObj2.fuelCalculator(1969);
         TyREObj2.fuelCalculator(100756);
@@ -51,14 +51,14 @@ public class TyrannyRocketEquationTask2Test {
     // Überge Wert, der Bufferoverflow erzeugt
 
     @Test
-    @Description("Call Method with Parameter > Range of int")
+    @DisplayName("Call Method with Parameter > Range of int")
     void fuelCalculatorTest_intOverflowParameter(){
 
 
     }
     // Übergebe mit verschiedenen Werten und Rückgabetypen
     @Test
-    @Description("Call Method with Mass of 12")
+    @DisplayName("Call Method with Mass of 12")
     void fuelCalculatorTest_TestCaseMass12(){
         Integer actual = TyREObj2.fuelCalculator(12);
         Integer expected = 2;
--- src/test/java/TyrannyRocketEquationTest.java
+++ src/test/java/TyrannyRocketEquationTest.java
@@ -1,6 +1,7 @@
 /* Get the Sources from: https://github.com/Gosbow/prog2-ss21-exercise1.git */
 
-import jdk.jfr.Description;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,7 +22,7 @@ public class TyrannyRocketEquationTest {
 
     // Übergebe der Methode eine negative Zahl
     @Test
-    @Description("Call Method with negative Numbers")
+    @DisplayName("Call Method with negative Numbers")
     void fuelCalculatorTest_MinusParameters(){
     TyREObj.fuelCalculator(-2);
     String expected = "ERROR: Mass is under 0";
@@ -31,21 +32,21 @@ public class TyrannyRocketEquationTest {
 
     // Übergebe der Methode eine Zahlen/Buchstaben Mischung
     @Test
-    @Description("Call Method with Numbers/Text Mix")
+    @DisplayName("Call Method with Numbers/Text Mix")
     void fuelCalculatorTest_Parameters(){
 
 
     }
     // Übergebe der Methode einen String
     @Test
-    @Description("Call Method a String as Parameter")
+    @DisplayName("Call Method a String as Parameter")
     void fuelCalculatorTest_StringParameters(){
 
 
     }
     // Übergebe der Methode Null
     @Test
-    @Description("Call Method with Null as Parameter")
+    @DisplayName("Call Method with Null as Parameter")
     void sumofCalcsTest_Sum654PLUS33583(){
         TyREObj.fuelCalculator(1969);
                 TyREObj.fuelCalculator(100756);
@@ -57,14 +58,14 @@ public class TyrannyRocketEquationTest {
     // Überge Wert, der Bufferoverflow erzeugt
 
     @Test
-    @Description("Call Method with Parameter > Range of int")
+    @DisplayName("Call Method with Parameter > Range of int")
     void fuelCalculatorTest_intOverflowParameter(){
     //Integer TyREObj.fuelCalculator("156156165156545646");
 
     }
     // Übergebe mit verschiedenen Werten und Rückgabetypen
     @Test
-    @Description("Call Method with Mass of 12")
+    @DisplayName("Call Method with Mass of 12")
     void fuelCalculatorTest_TestCaseMass12(){
         Integer actual = TyREObj.fuelCalculator(12);
         Integer expected = 2;
